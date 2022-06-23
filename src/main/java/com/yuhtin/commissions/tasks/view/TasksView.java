553967111d9d93ee5972ee72bfca1d494fa02b2a
Yuhtin <rio.daviduarte@gmail.com>
2022-06-22 22:13:43 -0300

    finalize view

--- src/main/java/com/yuhtin/commissions/tasks/view/TasksView.java
+++ src/main/java/com/yuhtin/commissions/tasks/view/TasksView.java
@@ -5,9 +5,16 @@ import com.henryfabio.minecraft.inventoryapi.item.InventoryItem;
 import com.henryfabio.minecraft.inventoryapi.item.supplier.InventoryItemSupplier;
 import com.henryfabio.minecraft.inventoryapi.viewer.impl.paged.PagedViewer;
 import com.yuhtin.commissions.tasks.TasksPlugin;
+import com.yuhtin.commissions.tasks.model.SimpleItem;
 import com.yuhtin.commissions.tasks.model.Task;
 import com.yuhtin.commissions.tasks.model.User;
+import com.yuhtin.commissions.tasks.util.ColorUtil;
+import com.yuhtin.commissions.tasks.util.EconomyHook;
 import com.yuhtin.commissions.tasks.util.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,10 +46,68 @@ public class TasksView extends PagedInventory {
             items.add(() -> InventoryItem.of(iconBuilder.wrap()).defaultCallback(callback -> {
                 if (completed) return;
 
-                // TODO
+                Player player = callback.getPlayer();
+                EconomyHook economyHook = TasksPlugin.getInstance().getEconomyHook();
+                if (!economyHook.has(player, task.getCoinsCost())) {
+                    player.sendMessage(ColorUtil.colored("&cVocê não tem dinheiro suficiente para completar esta missão."));
+                    return;
+                }
+
+                if (!economyHook.withdrawCoins(player, task.getCoinsCost()).transactionSuccess()) {
+                    player.sendMessage(ColorUtil.colored("&cVocê não tem dinheiro suficiente para completar esta missão."));
+                }
+
+                for (SimpleItem simpleItem : task.getItemCostList()) {
+                    if (!canConsume(player, simpleItem, false)) {
+                        player.sendMessage(ColorUtil.colored("&cVocê não tem itens suficientes para completar esta missão."));
+                        return;
+                    }
+                }
+
+                for (SimpleItem simpleItem : task.getItemCostList()) {
+                    canConsume(player, simpleItem, true);
+                }
+
+                for (String commands : task.getRewardCommandsList()) {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commands.replace("%player%", player.getName()));
+                }
+
+                user.getCompletedTasksIdList().add(task.getIdentifier());
+                player.sendMessage(ColorUtil.colored("&aVocê completou a missão &6" + task.getIdentifier() + "&a."));
             }));
         }
 
         return items;
+    }
+
+    public boolean canConsume(Player player, SimpleItem simpleItem, boolean consume) {
+        int count = simpleItem.getQuantity();
+
+        ItemStack[] items = player.getInventory().getContents();
+        for (ItemStack item : items) {
+            if (count < 1) break;
+            if (item == null || item.getType() == Material.AIR) continue;
+            if (item.getType().getId() != simpleItem.getItem().getTypeId()) continue;
+            if (item.getData().getData() != simpleItem.getItem().getData().getData()) continue;
+            if (!item.hasItemMeta()) continue;
+            if (!item.getItemMeta().hasDisplayName()) continue;
+            if (!item.getItemMeta().getDisplayName().equalsIgnoreCase(simpleItem.getItem().getItemMeta().getDisplayName()))
+                continue;
+
+            if (item.getAmount() > count) {
+                item.setAmount(item.getAmount() - count);
+                count = 0;
+
+                break;
+            } else {
+                count -= item.getAmount();
+                item.setAmount(0);
+            }
+        }
+
+        if (count > 0) return false;
+
+        if (consume) player.getInventory().setContents(items);
+        return true;
     }
 }
