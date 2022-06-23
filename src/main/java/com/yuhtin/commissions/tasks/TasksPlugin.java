c8e5d37f2e9bb9f692a212f382a744d880bec10c
Yuhtin <rio.daviduarte@gmail.com>
2022-06-22 22:13:22 -0300

    init managers

--- src/main/java/com/yuhtin/commissions/tasks/TasksPlugin.java
+++ src/main/java/com/yuhtin/commissions/tasks/TasksPlugin.java
@@ -3,6 +3,7 @@ package com.yuhtin.commissions.tasks;
 import com.yuhtin.commissions.tasks.cache.TasksCache;
 import com.yuhtin.commissions.tasks.command.TasksCommand;
 import com.yuhtin.commissions.tasks.manager.TasksManager;
+import com.yuhtin.commissions.tasks.util.EconomyHook;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,7 @@ public class TasksPlugin extends JavaPlugin {
 
     private TasksCache tasksCache;
     private TasksManager tasksManager;
+    private EconomyHook economyHook;
 
     @Override
     public void onLoad() {
@@ -19,8 +21,14 @@ public class TasksPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        economyHook = new EconomyHook();
+        economyHook.init();
+
         tasksCache = new TasksCache();
+        tasksCache.init();
+
         tasksManager = new TasksManager();
+        tasksManager.init();
 
         getCommand("tasks").setExecutor(new TasksCommand());
     }
